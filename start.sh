0f217a37e1518728b2a10053dcf86de038498651
narutooxox <112558957+narutooxox@users.noreply.github.com>
2022-09-11 23:22:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/narutooxox/Rolexv3 /EvaMaria
+  git clone https://github.com/narutooxox/v3togram.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
